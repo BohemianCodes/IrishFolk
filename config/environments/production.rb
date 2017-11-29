d803878b51918fad12067dccb68df5e19f1702a9
Bohemian Codes <chavelitaloca@gmail.com>
2017-11-29 13:31:02 +0100

    added gem devise and it's configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #configuation for devise
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'irishfolk.herokuapp.com' }
+
 end
