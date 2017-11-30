6ae5d413350031c43220e567b77364db7460bdc7
Bohemian Codes <chavelitaloca@gmail.com>
2017-12-01 00:23:14 +0100

    making app consumer ready

--- config/initializers/yt.rb
+++ config/initializers/yt.rb
@@ -1,3 +1,11 @@
-Yt.configure do |config|
-  config.api_key = 'AIzaSyB9Da5pjpLqbZQuGtEXlrZp_F8A_d2D7Hk'
+if Rails.env.production?
+
+  Yt.configure do |config|
+    config.api_key = ENV['YOUTUBE_API']
+  end
+
+else
+  Yt.configure do |config|
+    config.api_key = 'AIzaSyB9Da5pjpLqbZQuGtEXlrZp_F8A_d2D7Hk'
+  end
 end
